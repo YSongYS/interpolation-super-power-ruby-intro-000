9a856fbb2393382463e265bfa202f6b8beb0088b
Songyishu Yang <songyishu.yang@gmail.com>
2018-11-26 20:05:40 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -2,6 +2,6 @@
 def display_rainbow (colors)
   stringprint = ""
   colors.each {|color| stringprint << "#{color[0].upcase}: #{color}, "}
-  stringprint.chop.chop
-  print stringprint
+  stringprint = stringprint.chop.chop
+  puts stringprint
 end
