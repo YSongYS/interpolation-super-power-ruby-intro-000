17d27782fa3b3a8b2c90f214efbf3fd7e212fbda
Songyishu Yang <songyishu.yang@gmail.com>
2018-11-26 19:59:59 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,7 @@
 # Write your #display_rainbow method here
+def display_rainbow (colors)
+  stringprint = ""
+  colors.each {|color| stringprint << "#{color.initial.uppercase}: #{color}, "}
+  stringprint.chop
+  puts stringprint
+end
