aaef0b85ff392ed1e18e5c10635ecc3b9ce8cad4
Songyishu Yang <songyishu.yang@gmail.com>
2018-11-26 20:02:38 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,7 +1,7 @@
 # Write your #display_rainbow method here
 def display_rainbow (colors)
   stringprint = ""
-  colors.each {|color| stringprint << "#{color[0].uppercase}: #{color}, "}
+  colors.each {|color| stringprint << "#{color[0].upcase}: #{color}, "}
   stringprint.chop
   puts stringprint
 end
